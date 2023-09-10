534e82d19fb673ff0725e2d2b881b4a8301e3622
ss <99385518+soosiew@users.noreply.github.com>
2023-09-10 17:29:33 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -58,7 +58,7 @@ def addCompanyRegistration():
         # Close the cursor
         cursor.close()
 
-        company_id = ((int)result[0] + 1)
+        (int)company_id = ((int)result[0] + 1)
         company_name = request.form['company_name']
         company_image_file = request.files['company_image_file']
         about_company = request.form['about_company']
