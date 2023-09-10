56a0c5d29363fc63b69b29ec3938fcc06536acea
ss <99385518+soosiew@users.noreply.github.com>
2023-09-10 16:27:51 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -40,24 +40,89 @@ def manage_company_profile():
 def login_admin():
     return render_template('LoginAdmin.html')
 
+@app.route("/addCompanyReg", methods=['POST'])
+def addCompanyRegistration():
+    try:
+        # Create a cursor
+        cursor = db_conn.cursor()
+
+        # Execute the SELECT COUNT(*) query to get the total row count
+        select_sql = "SELECT COUNT(*) as total FROM company"
+        cursor.execute(select_sql)
+        result = cursor.fetchone()
+        
+        # Get the total row count from the result
+        total_count = result['total']
+
+        # Close the cursor
+        cursor.close()
+
+        company_id = total_count
+        company_name = request.form['company_name']
+        company_image_file = request.files['company_image_file']
+        about_company = request.form['about_company']
+        company_phone = request.form['company_phone']
+        company_address = request.form['company_address']
+        company_email = request.form['company_email']
+        password = request.form['password']
+
+        insert_sql = "INSERT INTO company VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor = db_conn.cursor()
+
+        if company_image_file.filename == "":
+            return "Please select a file"
+        
+        try:
+                cursor.execute(insert_sql, (company_id, password, company_name, about_company, company_address, company_email, company_phone, ""))
+                db_conn.commit()
+                
+                # Uplaod image file in S3 #
+                comp_image_file_name_in_s3 = "comp-id-" + str(company_id) + "_image_file"
+                s3 = boto3.resource('s3')
+
+                try:
+                    print("Data inserted in MySQL RDS... uploading image to S3...")
+                    s3.Bucket(custombucket).put_object(Key=comp_image_file_name_in_s3, Body=company_image_file)
+                    bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+                    s3_location = (bucket_location['LocationConstraint'])
+
+                    if s3_location is None:
+                        s3_location = ''
+                    else:
+                        s3_location = '-' + s3_location
+
+                    object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                        s3_location,
+                        custombucket,
+                        comp_image_file_name_in_s3)
+
+                except Exception as e:
+                    return str(e)
+        
+        finally:
+            cursor.close()
+
+        print("Company registration request submitted...")
+        return render_template('EditCompanyProfile.html', name=company_name)
+    
+    except Exception as e:
+        return render_template('EditCompanyProfile.html', name=company_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('AddEmp.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 @app.route("/about", methods=['POST'])
 def about():
