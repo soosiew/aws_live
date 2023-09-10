769efa2163f879e4b1bfeef2c40b010238000d45
soosiew <99385518+soosiew@users.noreply.github.com>
2023-09-10 22:36:30 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -77,11 +77,12 @@ def manage_company_profile():
 
         comp_name = company[2]
         comp_about = company[3]
+        print(comp_about) 
         comp_address = company[4]
         comp_email = company[5]
         comp_phone = company[6] 
 
-        print(comp_about)      
+             
 
         # Fetch the S3 image URL based on emp_id
         # emp_image_file_name_in_s3 = "comp-id-" + str(currentCompany) + "_image_file"
