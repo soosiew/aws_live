878eb21c7ea79e84675c99e3981ddd500552b64d
soosiew <99385518+soosiew@users.noreply.github.com>
2023-09-10 22:26:53 +0800

    a

--- EmpApp.py
+++ EmpApp.py
@@ -74,7 +74,7 @@ def manage_company_profile():
 
         if not company:
             print("company not found")
-
+        
         comp_name = company[2]
         comp_about = company[3]
         comp_address = company[4]
@@ -82,38 +82,29 @@ def manage_company_profile():
         comp_phone = company[6]       
 
         # Fetch the S3 image URL based on emp_id
-        emp_image_file_name_in_s3 = "comp-id-" + str(currentCompany) + "_image_file"
-        s3 = boto3.client('s3')
-        bucket_name = custombucket
+        # emp_image_file_name_in_s3 = "comp-id-" + str(currentCompany) + "_image_file"
+        # s3 = boto3.client('s3')
+        # bucket_name = custombucket
 
-        try:
-            response = s3.generate_presigned_url('get_object',
-                                                 Params={'Bucket': bucket_name,
-                                                         'Key': emp_image_file_name_in_s3},
-                                                 ExpiresIn=3600)  # Adjust the expiration time as needed
+        # try:
+        #     response = s3.generate_presigned_url('get_object',
+        #                                          Params={'Bucket': bucket_name,
+        #                                                  'Key': comp_image_file_name_in_s3},
+        #                                          ExpiresIn=3600)  # Adjust the expiration time as needed
+            
+            
 
-            # You can return the employee details along with the image URL
-            comp_details = {
-                # "emp_id": emp_id,
-                # "first_name": first_name,
-                # "last_name": last_name,
-                # "pri_skill": pri_skill,
-                # "location": location,
-                "image_url": response
-            }
+        #     return render_template('EditCompanyProfile.html',compName=comp_name, compLogo=response, compAbout=comp_about, compAddress=comp_address, compEmail=comp_email, compPhone=comp_phone)
 
-            return render_template('EditCompanyProfile.html',compName=comp_name, compLogo=response)
-
-        except Exception as e:
-            return str(e)
+        # except Exception as e:
+        #     return str(e)
 
     except Exception as e:
         return str(e)
 
     finally:
         cursor.close()
-        
-    
+        return render_template('EditCompanyProfile.html',compName=comp_name, compAbout=comp_about, compAddress=comp_address, compEmail=comp_email, compPhone=comp_phone)   
     
 
 @app.route('/login_admin')
